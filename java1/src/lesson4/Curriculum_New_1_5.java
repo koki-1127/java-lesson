7ddcb878a141d6dd5c928a9c7f558b1a52015e04
koki <eva.kk1127@gmail.com>
2023-04-21 18:19:51 +0900

    Java基礎　1.6 変数②　課題提出

--- java1/src/lesson4/Curriculum_New_1_5.java
+++ java1/src/lesson4/Curriculum_New_1_5.java
@@ -1,88 +1,47 @@
 package lesson4;
 
 public class Curriculum_New_1_5 {
-
 	public static void main(String[] args) {
-
-		// Q1 『』で囲われた人の情報を変数にして、formatの通りコンソールに出力してください
-		//  ローカル変数に代入し○○に入れてください
-		// 『山田太郎 18歳 170.5cm 62.2kg 寿司』
-
-		// ↓↓format↓↓
-		// 「初めまして○○です」
-		// 「年齢は○○歳です」
-		// 「身長は○○cmです」
-		// 「体重は○○kgです」
-		// 「好きな食べ物は○○です」
-
-		// Q1を下記に記載
-		String name, age, height, weight, like;
-		name = "山田太郎";
-		age = "１８歳";
-		height = "170.5㎝";
-		weight = "62.2kg";
-		like = "寿司";
-		System.out.println("初めまして" + name + "です");
-		System.out.println("年齢は" + age + "です");
-		System.out.println("身長は" + height + "です");
-		System.out.println("体重は" + weight + "です");
-		System.out.println("好きな食べ物は" + like + "です");
-
-		// Q2 Q1で作成した自己紹介に続いて、BMIがコンソールに出力されるようにしてください
-		// 「BMIは○○です」
-		// ただし計算は数値を直書きせず、全て変数を使ってすること
-
-		// Q2を下記に記載
-		double height2, weight2;
-		height2 = 1.70;
-		weight2 = 62;
-		double d1 = weight2 / (height2 * height2);
-		double roundDb1 = Math.floor(d1 * 10.0) / 10.0;
-		System.out.println("BMIは" + roundDb1 + "です");
+//		問１）
+//		変数 x を宣言し、「3」を代入して下さい。
+//		変数 x に自己代入を使って「20」を掛け算して下さい。
+//		その後 x の値をコンソールに表示して下さい。
+		
+//		問１）の記述↓
+		int x = 3;
+		x = x *= 20;
+		System.out.println("変数xの値：" + x); 
 		System.out.println();
 
-		// Q3 Q1で宣言した変数に再代入し、下記の通りコンソールに出力してください
-		// 初めまして鈴木一郎です
-		// 年齢は24歳です
-		// 身長168.5cmです
-		// 体重は64.2kgです
-		// 好きな食べ物はオムライスです
-		// BMIは22.6です
+//		問２）
+//		以下のプログラムを作成してください。
+//		①int 型の変数 i に 100 を代入する。
+//		②インクリメント演算子を適用して数値を 1 増やす。
+//		③コンソールに変数 i の値を表示する。
+//		④デクリメント演算子を適用して数値を 1 減らす。
+//		⑤コンソールに変数 i の値を表示する。
 
-		// Q3を下記に記載
-		name = "鈴木一郎";
-		age = "24歳";
-		height = "168.5㎝";
-		weight = "64.2kg";
-		like = "オムライス";
-		height2 = 1.68;
-		weight2 = 64;
-		System.out.println("初めまして" + name + "です");
-		System.out.println("年齢は" + age + "です");
-		System.out.println("身長は" + height + "です");
-		System.out.println("体重は" + weight + "です");
-		System.out.println("好きな食べ物は" + like + "です");
+//		問２）の記述↓
+		int i = 100;
+		i++;
+		System.out.println("i++値出力結果：" + i);
+		i--;
+		System.out.println("i--値出力結果：" + i);
+		System.out.println();
 
-		double d2 = weight2 / (height2 * height2);
-		double roundDb2 = Math.floor(d2 * 10.0) / 10.0;
-		System.out.println("BMIは" + roundDb2 + "です");
-
-		// ↓↓コンソール最終結果↓↓
-
-		//	初めまして山田太郎です
-		//	年齢は18歳です
-		//	身長170.5cmです
-		//	体重は62.2kgです
-		//	好きな食べ物は寿司です
-		//	BMIは21.4です
-		//	
-		//	初めまして鈴木一郎です
-		//	年齢は24歳です
-		//	身長168.5cmです
-		//	体重は64.2kgです
-		//	好きな食べ物はオムライスです
-		//	BMIは22.6です
+//		問３）
+//		下記の例のような文章をコンソールに出力して下さい。
+//		但し、年齢の部分は乱数を使用し実行毎に結果が変わるようにして下さい。
+//		※数字は0～100までの間で表示されるようにしてください。
+//		例）
+//		あなたの年齢を予想します。
+//		あなたの年齢はおそらく〇〇歳ですね？
+		
+//		問３）の記述↓
+		double double_number = Math.random()*100+ 1;
+        int int_number =(int)double_number;
+        System.out.println("あなたの年齢を予想します。");
+        System.out.println("あなたの年齢はおそらく" + int_number + "歳ですね？");
 
 	}
-
 }
