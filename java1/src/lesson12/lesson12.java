7808e03d1a14bc6d2d0ad7f9cbebcee48dd1f29d
kouki <eva.kk1127@gmail.com>
2023-04-28 15:46:39 +0900

    1_12 課題提出

---
+++ java1/src/lesson12/lesson12.java
@@ -0,0 +1,35 @@
+package lesson12;
+
+public class lesson12 {
+
+	public static void main(String[] args) {
+		/*
+		問題．while文を使用し、以下のプログラムを作成してください。
+		・1～100を出力
+		・奇数の数字に「〇は奇数です」と出力
+		・偶数の数字に「○は偶数です」と出力
+		出力結果  1は奇数です
+				  2は偶数です
+				  3は奇数です
+				  4は偶数です
+					  ・
+					  ・
+					  ・
+				  100は偶数です
+		 */
+		
+		int i = 1;
+		do {
+			//奇数の判定
+			if ( 0 != ( i % 2 ) ) {
+				System.out.println( i + "は奇数です");
+			}else {
+				System.out.println( i + "は偶数です");
+			}
+			//iに1を足していく
+			i++;
+		}
+		//100に到達したら処理終了
+		while ( 100 >= i );
+	}
+}
