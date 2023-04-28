f7ea34308aa277f7b27f1d13ae9201c48d2f3d1e
kouki <eva.kk1127@gmail.com>
2023-04-28 14:34:06 +0900

    1_8 再提出

--- java1/src/lesson8/lesson8.java
+++ java1/src/lesson8/lesson8.java
@@ -31,16 +31,16 @@ public class lesson8 {
 		int e = 12;
 		int f = 8;
 		boolean result_3;
-		result_3 = (e > 10) && (f <= 10);
+		result_3 = (e > 10) && (f < 10);
 		System.out.println(result_3);
-
+		
 		//問４）
 		//以下の変数を使ってboolean型変数 result_4 に
 		//「変数 g の値は5以下 または 変数 g の値は10以上」という条件式を代入して下さい。
 		//boolean型変数 result_4 をコンソールに出力して下さい。
 		int g = 7;
 		boolean result_4;
-		result_4 = (g < 5) || (g > 10);
+		result_4 = (g <= 5) || (g >= 10);
 		System.out.println(result_4);
 	}
 }
