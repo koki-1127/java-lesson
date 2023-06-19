7007c86973f1b954c4706bceeac106c7a944b27f
kouki <eva.kk1127@gmail.com>
2023-06-20 01:12:04 +0900

    カリキュラム１９提出

--- java1/src/basicClass/Animal.java
+++ java1/src/basicClass/Animal.java
@@ -6,19 +6,19 @@ import java.util.Date;
 public class Animal {
 
 	public static void main(String[] args) {
-		
+
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		Dog dog_name = new Dog();
 		System.out.println(dog_name.name);
-		
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		Dog dog_num = new Dog();
 		System.out.println(dog_num.num);
-		
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		Date dateObj = new Date();
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd H:m:s");
-		
+
 		// 指定の形式で取得の後、出力
 		String display = format.format(dateObj);
 		System.out.println(display);
