f3d6813b5adac51d3868afff76ee0390e37c4a34
kouki <eva.kk1127@gmail.com>
2023-07-07 00:45:28 +0900

    課題１９再提出

--- java1/src/basicClass/Animal.java
+++ java1/src/basicClass/Animal.java
@@ -8,12 +8,12 @@ public class Animal {
 	public static void main(String[] args) {
 
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		Dog dog_name = new Dog(null);
-		System.out.println(dog_name.name);
+		Dog dogName = new Dog(null);
+		System.out.println(dogName.name);
 
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		Dog dog_num = new Dog(0);
-		System.out.println(dog_num.num);
+		Dog dogNum = new Dog(0);
+		System.out.println(dogNum.num);
 
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		Date dateObj = new Date();
